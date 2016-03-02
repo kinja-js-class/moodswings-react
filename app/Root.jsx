6c39d1bba36db0e5c429fe94f3f8de3dc31302b8
Gáspár Körtesi <gazs@gawker.com>
2016-03-02 17:46:22 +0100

    fireducks

--- app/Root.jsx
+++ app/Root.jsx
@@ -1,16 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { createStore, compose } from 'redux'
+import { createStore, compose, applyMiddleware, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+
 
 import DevTools from './devtools'
 
 import { moodswingsApp } from './reducers/reducers'
 
-let store = compose( DevTools.instrument())(createStore)(moodswingsApp)
 
 import App from './app.jsx'
+
+import Firedux from 'firedux'
+
+const firedux = new Firedux({
+  url: 'https://amber-fire-5100.firebaseio.com/',
+
+  // Optional:
+  omit: ['$localState'] // Properties to reserve for local use and not sync with Firebase.
+})
+
+const reducer = combineReducers({
+  firedux: firedux.reducer(),
+	moodswingsApp: moodswingsApp
+})
+
+
+// create store with middleware, including thunk.
+const store = compose(applyMiddleware(
+	thunk
+),
+DevTools.instrument())
+(createStore)(reducer)
+
+
+firedux.dispatch = store.dispatch
+
+firedux.init()
+
+firedux.watch('moodValue')
+window.firedux = firedux
+
 
 class Root extends React.Component {
 	render () {
